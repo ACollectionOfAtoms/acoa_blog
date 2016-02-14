0b1bb48981ec1110f1f6b27b25ffd6c0bdc65d1a
Adam Hernandez <hernandezdavid88@utexas.edu>
2016-02-14 12:03:42 -0600

    Part VI

--- app/views.py
+++ app/views.py
@@ -2,8 +2,9 @@ from flask import render_template, flash, redirect, url_for, session, request, g
 from flask_login import login_user, logout_user, current_user, login_required
 from app import app, lm, db
 from app.OAuthSignIn import OAuthSignIn
-from .forms import LoginForm
 from .models import User
+from datetime import datetime
+from .forms import EditForm
 
 
 @app.route('/')
@@ -28,6 +29,7 @@ def index():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
 
 @app.route('/authorize/<provider>')
 def oauth_authorize(provider):
@@ -58,8 +60,45 @@ def oauth_callback(provider):
 @app.before_request
 def before_request():
     g.user = current_user
+    if g.user.is_authenticated:
+        g.user.last_seen = datetime.utcnow()
+        db.session.add(g.user)
+        db.session.commit()
 
 
 @lm.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user == None:
+        flash('User %s not found.' % nickname)
+        return redirect(url_for('index'))
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author':user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html',
+                           user=user,
+                           posts=posts)
+
+
+@app.route('/edit', methods=['GET', 'POST'])
+@login_required
+def edit():
+    form = EditForm()
+    if form.validate_on_submit():
+        g.user.nickname = form.nickname.data
+        g.user.about_me = form.about_me.data
+        db.session.add(g.user)
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit'))
+    else:
+        form.nickname.data = g.user.nickname
+        form.about_me.data = g.user.about_me
+    return render_template('edit.html', form=form)
